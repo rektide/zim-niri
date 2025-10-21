33ab08d81862f11a9e0e7e9391f7f25bf9f98f60
rektide de la faye <rektide@voodoowarez.com>
2025-10-20 22:47:42 -0400

    initial zim-niri

---
+++ README.md
@@ -0,0 +1,3 @@
+# zim-niri
+
+> [Zimfw](https://zimfw.sh/) module to initialize [niri shell completions](https://github.com/YaLTeR/niri/wiki/Packaging-niri#shell-completions).
---
+++ init.zsh
@@ -0,0 +1,11 @@
+# framing by-way-of zim-mise, passing in directory holding the script
+(( ${+commands[niri]} )) && () {
+  local command=${commands[niri]}
+
+    # generating completions
+  local compfile=$1/functions/_niri
+  if [[ ! -e $compfile || $compfile -ot $command ]]; then
+    $command complete --shell zsh >| $compfile
+    print -u2 -PR "* Detected a new version 'niri'. Regenerated completions."
+  fi
+} ${0:h}  
